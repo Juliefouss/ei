db9253e9930a66d7bf4c73198af42264dab9537c
juliefouss <foussy89@hotmail.com>
2022-09-02 09:06:15 +0200

    messages

---
+++ src/Controller/ContactMessageController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ContactMessage;
+use App\Form\ContactMessage1Type;
+use App\Repository\ContactMessageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/contact/message')]
+class ContactMessageController extends AbstractController
+{
+    #[Route('/', name: 'app_contact_message_index', methods: ['GET'])]
+    public function index(ContactMessageRepository $contactMessageRepository): Response
+    {
+        return $this->render('pages/admin/contact_message/index.html.twig', [
+            'contact_messages' => $contactMessageRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_contact_message_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ContactMessageRepository $contactMessageRepository): Response
+    {
+        $contactMessage = new ContactMessage();
+        $form = $this->createForm(ContactMessage1Type::class, $contactMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactMessageRepository->add($contactMessage, true);
+
+            return $this->redirectToRoute('app_contact_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('contact_message/new.html.twig', [
+            'contact_message' => $contactMessage,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_contact_message_show', methods: ['GET'])]
+    public function show(ContactMessage $contactMessage): Response
+    {
+        return $this->render('pages/admin/contact_message/show.html.twig', [
+            'contact_message' => $contactMessage,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_contact_message_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, ContactMessage $contactMessage, ContactMessageRepository $contactMessageRepository): Response
+    {
+        $form = $this->createForm(ContactMessage1Type::class, $contactMessage);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactMessageRepository->add($contactMessage, true);
+
+            return $this->redirectToRoute('app_contact_message_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('contact_message/edit.html.twig', [
+            'contact_message' => $contactMessage,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_contact_message_delete', methods: ['POST'])]
+    public function delete(Request $request, ContactMessage $contactMessage, ContactMessageRepository $contactMessageRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contactMessage->getId(), $request->request->get('_token'))) {
+            $contactMessageRepository->remove($contactMessage, true);
+        }
+
+        return $this->redirectToRoute('app_contact_message_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/ContactMessage1Type.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ContactMessage;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactMessage1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('subject')
+            ->add('message')
+            ->add('firstname')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactMessage::class,
+        ]);
+    }
+}
